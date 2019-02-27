7a6ac72820b7d49909e86ada3d792bcefa4b5fce
wananyx <412828823@qq.com>
2019-02-27 22:22:58 +0800

    改包名

---
+++ adx-admin/src/main/java/com/yx/adx/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,53 @@
+package com.yx.adx.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.yx.adx.dao.ArticleDao;
+import com.yx.adx.service.ArticleService;
+import com.yx.adx.util.CommonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author: hxy
+ * @date: 2017/10/24 16:07
+ */
+@Service
+public class ArticleServiceImpl implements ArticleService {
+
+	@Autowired(required = true)
+	private ArticleDao articleDao;
+
+	/**
+	 * 新增文章
+	 */
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public JSONObject addArticle(JSONObject jsonObject) {
+		articleDao.addArticle(jsonObject);
+		return CommonUtil.successJson();
+	}
+
+	/**
+	 * 文章列表
+	 */
+	@Override
+	public JSONObject listArticle(JSONObject jsonObject) {
+		CommonUtil.fillPageParam(jsonObject);
+		int count = articleDao.countArticle(jsonObject);
+		List<JSONObject> list = articleDao.listArticle(jsonObject);
+		return CommonUtil.successPage(jsonObject, list, count);
+	}
+
+	/**
+	 * 更新文章
+	 */
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public JSONObject updateArticle(JSONObject jsonObject) {
+		articleDao.updateArticle(jsonObject);
+		return CommonUtil.successJson();
+	}
+}
